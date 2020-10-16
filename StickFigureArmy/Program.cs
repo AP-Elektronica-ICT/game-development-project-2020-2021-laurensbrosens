09703aa42cbfc0c18b2b24b3c8489cea40d4e1d8
laurens <laurens.brosens@student.ap.be>
2020-10-16 16:45:37 +0200

    Added Monogame project

---
+++ StickFigureArmy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StickFigureArmy
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
